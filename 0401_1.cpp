197f88522747bd0c62b81c7ff41c094c0c1db13d
Yang470404 <sjy470404@gmail.com>
2025-04-01 09:16:08 +0900

    0401_정수형실수형 출력프로그램

---
+++ 0401_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	float Number4 = 1.3;
+	float Number5 = 2.5;
+
+	float Number6 = Number4 + Number5;
+
+	cout << Number6 << endl;
+
+	return 0;
+}
---
+++ 0401_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	int Number1 = 100;
+	int Number2 = 101;
+
+	int Number3 = Number1 + Number2;
+	cout << Number3 << endl;
+
+	return 0;
+}
